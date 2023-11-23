93029e210a7284fc86f6febaefee75fbcaddbe1d
Sarah Laurin <sarahno.laurin@gmail.com>
2023-11-23 12:45:49 -0500

    rinse and repeat corruption and prefab setup for other 2 memories

--- Assets/Scripts/Corruption/CorruptionFuneral.cs
+++ Assets/Scripts/Corruption/CorruptionFuneral.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CorruptionHospital : MonoBehaviour
+public class CorruptionFuneral : MonoBehaviour
 {
     /// <summary>
     /// Determines when objects should or shouldn't be interactable depending on where they are according to the corruption
     /// </summary>
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,13 @@ public class CorruptionHospital : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     /*
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("HospitalInteractable"))
+        if (other.CompareTag("FuneralInteractable"))
         {
             other.transform.GetChild(0).GetComponent<VisualTrigger>().isCorrupted = false;
           
@@ -34,8 +35,8 @@ public class CorruptionHospital : MonoBehaviour
     //Anything inside this object is not corrupted
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("HospitalInteractable"))
-        {            
+        if (other.CompareTag("FuneralInteractable"))
+        {
             other.transform.GetChild(0).GetComponent<VisualTrigger>().isCorrupted = false;
             //Debug.Log("No corruption");
         }
@@ -44,7 +45,7 @@ public class CorruptionHospital : MonoBehaviour
     //Anything outside this object is corrupted
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("HospitalInteractable"))
+        if (other.CompareTag("FuneralInteractable"))
         {
             other.transform.GetChild(0).GetComponent<VisualTrigger>().isCorrupted = true;
             Debug.Log("Start corruption");
